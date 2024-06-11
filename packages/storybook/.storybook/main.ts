fb0a90e13b206ecb53f9a4139845857984fe1c43
Parthiban Baskar <parthi.krb@gmail.com>
2024-06-11 22:12:53 +0200

    add biome

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -6,7 +6,7 @@ import { join, dirname } from "node:path";
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-// @ts-ignore
+// biome-ignore lint/suspicious/noExplicitAny: This function is used to resolve the absolute path of a package.
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
