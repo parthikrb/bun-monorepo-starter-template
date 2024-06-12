e5306309ed657e8c8f03469b702a9d34117a3e2c
Parthiban Baskar <parthi.krb@gmail.com>
2024-06-12 17:32:05 +0200

    formatting

--- packages/storybook/.storybook/main.ts
+++ packages/storybook/.storybook/main.ts
@@ -1,5 +1,5 @@
-import type { StorybookConfig } from '@storybook/react-vite'
 import { dirname, join } from 'node:path'
+import type { StorybookConfig } from '@storybook/react-vite'
 
 /**
  * This function is used to resolve the absolute path of a package.
